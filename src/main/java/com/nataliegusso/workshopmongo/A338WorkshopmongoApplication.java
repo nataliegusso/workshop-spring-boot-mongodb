da1416c2f270e27b7cedcf2a246e857ef9434e4b
nataliegusso <nataliegusso@gmail.com>
2020-05-19 16:49:31 -0300

    Projeto criado

---
+++ src/main/java/com/nataliegusso/workshopmongo/A338WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.nataliegusso.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A338WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A338WorkshopmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nataliegusso/workshopmongo/A338WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nataliegusso.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A338WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
