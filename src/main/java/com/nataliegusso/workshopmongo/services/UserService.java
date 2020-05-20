c447f6104abca9d0ff4a9f7640e11a2a687837ed
nataliegusso <nataliegusso@gmail.com>
2020-05-20 17:12:42 -0300

    Atualização de usuário com PUT

--- src/main/java/com/nataliegusso/workshopmongo/services/UserService.java
+++ src/main/java/com/nataliegusso/workshopmongo/services/UserService.java
@@ -35,6 +35,17 @@ public class UserService {
 		repo.deleteById(id);
 	}
 	
+	public User update(User obj) {  //O que o usuário digitou na tela
+		User newObj = findById(obj.getId()); //pega no BD
+		updateData(newObj, obj);  //atualiza (ver abaixo)
+		return repo.save(newObj);  //salva o digitado no BD
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+	
 	public User fromDTO(UserDTO objDto) {  //Pega um dto e instancia um usuário user  //Poderia ser feito na classe UserDTO, mas aqui já tem dependência no BD, o que facilita
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
